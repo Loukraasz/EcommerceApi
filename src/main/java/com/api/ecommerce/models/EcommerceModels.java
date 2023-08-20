7c244fad61c3b024db8aff12b89878c7a5970753
Loukraasz <peixotolukas06@gmail.com>
2023-08-20 13:35:15 -0300

    commit inicial

---
+++ src/main/java/com/api/ecommerce/models/EcommerceModels.java
@@ -0,0 +1,5 @@
+package com.api.ecommerce.models;
+
+public class EcommerceModels {
+    
+}
---
+++ src/main/java/com/api/ecommerce/repository/EcommerceRepository.java
@@ -0,0 +1,5 @@
+package com.api.ecommerce.repository;
+
+public interface EcommerceRepository {
+    
+}
